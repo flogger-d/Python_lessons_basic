7fa66303a993255471c7884c44c89d848c625d16
Milonov Grigory <Grigory.Milonov@infotecs.ru>
2019-03-12 10:19:00 +0300

    Easy - работает. Normal - не получилось с импортом.

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -1,10 +1,80 @@
+#!/usr/bin/python3
+# encoding utf-8
+
+
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
-# Задача-2:
-# Напишите скрипт, отображающий папки текущей директории.
+if __name__ == "__main__":
+     import os
+     import sys
 
-# Задача-3:
-# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+def ls_func():
+     try:
+          for root, dirs, files in os.walk('.'):
+
+               for d in dirs.sort():
+                    print(" {}".format(d))
+
+               for d in files.sort():
+                    print(" {}".format(d))
+
+     except Exception as e:
+          print('Ошибка: '+str(e))
+
+def ls_dirs_func():
+     try:
+          for root, dirs, files in os.walk('.'):
+               for d in dirs:
+                    print(" - {}".format(d))
+     except Exception as e:
+          print('Ошибка: '+str(e))
+
+if __name__ == "__main__":
+     print("Задача #1")
+     try:
+          input('Создадим папки dir_1 - dir_9 в текущем каталоге')
+          path = os.getcwd()
+          mkdirs = [os.path.join(path,'dir_'+str(d)) for d in range(10)]
+          for d in mkdirs:
+               if not os.path.exists(d):
+                    os.mkdir(d)
+
+          ls_dirs_func()
+
+          input('А теперь удалим их')
+
+          for d in mkdirs:
+               os.rmdir(d)
+
+          ls_dirs_func()
+
+     except Exception as e:
+          print("Ошибка: " + str(e))
+
+
+     # Задача-2:
+     # Напишите скрипт, отображающий папки текущей директории.
+
+     print("Задача #2")
+     input("В текущей директории есть следующие папки:")
+     ls_dirs_func()
+
+
+     # Задача-3:
+     # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+     print("Задача #3")
+
+     myself = sys.argv[0]
+     print("Скрипт запущен из файла '"+myself+"'")
+
+     myself_copy = myself+".copy"
+     print("Файл будет скопирован в '"+myself_copy+"'")
+
+     os.system('cp ' + myself + ' ' + myself_copy)
+
+
+
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# enconding utf-8
+
 # Задача-1:
 # Напишите небольшую консольную утилиту,
 # позволяющую работать с папками текущей директории.
@@ -13,3 +16,92 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+
+import os
+import sys
+
+from .hw05_easy import ls_func
+
+
+Run = True
+
+def Pwd():
+     print("Текущий каталог: "+os.getcwd())
+
+def Cd():
+     path = input("Куда перейти: ")
+     try:
+          os.chdir(path)
+          print("Ok")
+     except Exception as e:
+          print("Ошибка: " + str(e))
+
+def Ls():
+     print("======ТЕКУЩИЙ КАТАЛОГ===")
+     hw05_easy.ls_func()
+     print("======КОНЕЦ=============")
+
+def MkDir():
+     path = input("Создать каталог. Введите имя каталога: ")
+     try:
+          os.mkdirs(path)
+          print("Ok")
+     except Exception as e:
+          print("Ошибка создания каталога: "+str(e))
+
+def RmDir():
+     path = input("Удалить каталог. Введите имя каталога: ")
+     try:
+          os.rmdir(path)
+          print("Ok")
+     except Exception as e:
+          print("Ошибка удаления каталога: "+str(e))
+
+def Exit():
+     print("До свидания!")
+     global Run
+     Run = False
+
+text = [
+     "Текущий каталог",
+     "Перейти в новый каталог",
+     "Содержимое текущего каталога",
+     "Создать каталог",
+     "Удалить каталог (должен быть пустым!)",
+     "Выход из программы"
+]
+
+handlers = [
+     Pwd,
+     Cd,
+     Ls,
+     MkDir,
+     RmDir,
+     Exit
+]
+
+
+commands = [[i, t, h] for i, t, h in zip(range(1,len(text)+1), text, handlers) ]
+
+def REPL():
+     print("Вам доступны следующие действия: ")
+     for  i,t,_  in commands:
+          print("{}. {}".format(i,t))
+     try:
+          i = int(input("Введите индекс команды: "))
+          if i in range(1,len(commands)+1):
+               print("\n\n{}:\n".format(commands[i-1][1]))
+               commands[i-1][2]()
+               print("\n")
+          else:
+               print("'"+i+"': Команда не найдена!")
+     except Exception as e:
+          print("Ошибка: "+str(e))
+          print("Нужно ввести число из указанного диапазона")
+
+if __name__ == "__main__":
+     while Run:
+          REPL()
+
+
